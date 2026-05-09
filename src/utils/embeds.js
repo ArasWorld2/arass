8b05bac01f8509b2594f557ceddd7923c75be04d
Aras <parkaras192@gmail.com>
2026-05-09 18:59:44 +0100

    bigger flight number

--- src/utils/embeds.js
+++ src/utils/embeds.js
@@ -33,7 +33,7 @@ function buildMainEmbed(flight, allocation) {
     .addFields(
       {
         name: '🛫  DEPARTURE DUTY',
-        value: `**${flight.number}**`,
+        value: `# ${flight.number}`,
       },
       {
         name: '\u200B',
