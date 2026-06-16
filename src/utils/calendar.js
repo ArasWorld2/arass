f7d31dfb9eca0e08b6ffb4fa19d5b0f1eabb3759
Aras <parkaras192@gmail.com>
2026-06-16 18:43:56 +0100

    style: convert hardcoded countdown text into relatidsadsadasve hammer time markup

--- src/utils/calendar.js
+++ src/utils/calendar.js
@@ -182,7 +182,7 @@ async function checkUpcomingDepartures(client) {
           `### <:AIRDOMplane:1480019019556847796> Scheduled Flight\n` +
           `-# <:AIRDOM_blank:1512890372865396746> \`${formattedDate}\` <:calender:1414278015440912415> \n\n` +
           `> We would like to remind you that flight **${cleanEventName}** is scheduled to depart **${relativeHammerTime}**. For your convenience, all **relevant details for the departure** may be found in the event card shared below. If you have any inquiries or concerns about the upcoming itinerary, please don't hesitate to let us know through contacting **<@1480328342552182835>**.\n` +
-          `<:AIRDOMARR:1480207668504297745> Please be advised that you must be a member of our [**Roblox Group**](<https://www.roblox.com/communities/35331638/Air-Dolomiti-Virtual#!/about>) to join flights. On behalf of **Wizz Air**, we wish you a pleasant journey.\n\n` +
+          `<:AIRDOMARR:1480207668504297745> Please be advised that you must be a member of our [**Roblox Group**](<https://www.roblox.com/communities/35331638/Air-Dolomiti-Virtual#!/about>) to join flights. On behalf of **Air Dolomiti**, we wish you a pleasant journey.\n\n` +
           `-# <:AIRDOMLINK:1480194781333164113> [**${cleanEventName}**](<${event.url}>)`;
 
         await departuresChannel.send({ 
