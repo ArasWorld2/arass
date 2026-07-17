f57acdefa0eedf0143d260616729d8eb6ffa4b55
Aras <parkaras192@gmail.com>
2026-07-17 17:13:16 +0100

    style: update academy invite embed side-strip color to #d3007f

--- src/commands/academyinvite.js
+++ src/commands/academyinvite.js
@@ -33,7 +33,7 @@ module.exports = {
 
         // 3. Build the Wizz Air Embed Layout
         const inviteEmbed = new EmbedBuilder()
-            .setColor('#d3007f') // Wizz-themed purple color (change hex code if you want pink/magenta)
+            .setColor('#d3007f') // Your new custom hex color applied here!
             .setTitle('<:care:1414277804555632801> Application Status')
             .setDescription('<:blank:1296498991114227763> ``Fly Greenest`` <:flygreen:1272674839441965056>\n\n> Congratulations, on behalf of the **Wizz Recruitment Department**, we are pleased to announce that you have passed our direct entry opportunity and you\'re officially cleared to start building your career with us here at Wizz.\n\n<:arrow:1414277373909794937> To begin your career, we have an extensive two weeks prepared ahead for you whilst presenting one of the most challenging yet realistic training programs, designed individually depending on each department and different strengths. Once again, **congratulations**!\n\n> <:click:1414277937187782818> **[Academy Invitation](https://discord.gg/GZPhEGTBEA)**\n> -# This link is strictly protected under our internal regulations, and leaking to third-parties will result in a **blacklist** from our organisation. Ensure that you follow the instructions provided to verify and continue your journey within the academy.')
             .setFooter({ 
@@ -41,14 +41,11 @@ module.exports = {
                 iconURL: interaction.guild.iconURL() // Pulls your server icon automatically
             });
 
-        const signOffText = `<:heart:1414277635126591579> **Viszlát**`;
-
-        // 4. Loop through each user ID and attempt to DM them the embed
+        // 4. Loop through each user ID and attempt to DM them the embed only
         for (const id of userIds) {
             try {
                 const user = await interaction.client.users.fetch(id);
-                // Embeds sent via DM work best when accompanied by a clean sign-off text outside the block
-                await user.send({ content: signOffText, embeds: [inviteEmbed] });
+                await user.send({ embeds: [inviteEmbed] });
                 successCount++;
             } catch (err) {
                 console.error(`Failed to send embed invite to ID ${id}:`, err.message);
