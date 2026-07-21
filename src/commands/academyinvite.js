ffdc0bdddda6ffd942c2e94f431be46953c224da
Aras <parkaras192@gmail.com>
2026-07-21 22:29:59 +0100

    fix: render academy invite hyperlink correctly in embed description

--- src/commands/academyinvite.js
+++ src/commands/academyinvite.js
@@ -34,19 +34,17 @@ module.exports = {
         let successCount = 0;
         let failedUsers = [];
 
-        // Build the Wizz Air Embed Layout
+        // Build the Wizz Air Embed Layout with proper Markdown parsing
         const inviteEmbed = new EmbedBuilder()
             .setColor('#D3007F')
             .setTitle('<:care:1414277804555632801> Application Status')
             .setDescription(
                 `<:blank:1296498991114227763> \`Fly Greenest\` <:flygreen:1272674839441965056>\n\n` +
                 `> Congratulations, on behalf of the **Wizz Recruitment Department**, we are pleased to announce that you have passed our direct entry opportunity and you're officially cleared to start building your career with us here at Wizz.\n\n` +
-                `<:arrow:1414277373909794937> To begin your career, we have an extensive two weeks prepared ahead for you whilst presenting one of the most challenging yet realistic training programs, designed individually depending on each department and different strengths. Once again, **congratulations**!`
+                `<:arrow:1414277373909794937> To begin your career, we have an extensive two weeks prepared ahead for you whilst presenting one of the most challenging yet realistic training programs, designed individually depending on each department and different strengths. Once again, **congratulations**!\n\n` +
+                `<:click:1414277937187782818> **[Academy Invitation](https://discord.gg/GZPHEGTBEA)**\n` +
+                `> This link is strictly protected under our internal regulations, and leaking to third-parties will result in a **blacklist** from our organisation. Ensure that you follow the instructions provided to verify and continue your journey within the academy.`
             )
-            .addFields({
-                name: '<:click:1414277937187782818> [Academy Invitation](https://discord.gg/GZPHEGTBEA)',
-                value: '> This link is strictly protected under our internal regulations, and leaking to third-parties will result in a **blacklist** from our organisation. Ensure that you follow the instructions provided to verify and continue your journey within the academy.'
-            })
             .setFooter({
                 text: 'Onboarding Office, Recruitment Department • Wizz Air, Fly Greenest',
                 iconURL: interaction.guild?.iconURL() || undefined
@@ -57,7 +55,6 @@ module.exports = {
             try {
                 const user = await interaction.client.users.fetch(id);
                 
-                // Send only the embed (Viszlát text removed)
                 await user.send({ embeds: [inviteEmbed] });
                 successCount++;
             } catch (err) {
